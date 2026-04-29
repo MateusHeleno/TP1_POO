ff52dd648157bee56308f66b11cd5381760ce5ed
Joao-Felipe-Paiva-Paixao <jfera2007.jfpp@gmail.com>
2026-04-29 14:21:19 -0300

    Mudancas incompletas

--- transacao.cpp
+++ transacao.cpp
@@ -3,60 +3,96 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include<string>
+#include <string>
 
 Transacao::Transacao(string t, double v, string d, string h)
     : tipo(t),
-    valor(v),
-    data(d),
-    horario(h)
-     
+      valor(v),
+      data(d),
+      horario(h)
+
 {
     // a classe foi inicializada acima
 }
 
-string Transacao::getTipo(){
+string Transacao::getTipo()
+{
 
     return tipo;
-
 }
 
-double Transacao::getValor(){
+double Transacao::getValor()
+{
 
     return valor;
-
 }
 
-void Transacao::setValor(double v){
+void Transacao::setValor(double v)
+{
 
-    if(v >= 0.001){
+    if (v >= 0.001)
+    {
         valor = v;
-
     }
-
+    else
+        cout << "Erro: Valor invalido";
 }
 
-string Transacao::getData(){
+string Transacao::getData()
+{
 
     return data;
-
 }
 
-void Transacao::setData(string d){
+void Transacao::setData(string d)
+{
 
     data = d;
-
 }
 
-string Transacao::getHorario(){
+string Transacao::getHorario()
+{
 
     return horario;
-
 }
 
-void Transacao::setHorario(string h){
+void Transacao::setHorario(string h)
+{
 
     horario = h;
-
 }
 
+//void Transacao::setClientes(Cliente *c)
+
+vector<Cliente *> Transacao::getClientes()
+{
+    return clientesEnvolvidos; // ja eh a colecao de clientes envolvidos completa
+}
+
+void Transacao::exibirTransacao()
+{
+    cout << "Tipo: " << tipo << endl
+         << "Valor: R$ " << valor << endl
+         << "Data: " << data << endl
+         << "Horario: " << horario << endl;
+
+    cout << "Clientes Envolvidos:" << endl; // impressao dos clientes
+
+    if (clientesEnvolvidos.empty()) // verifica se a lista esta vazia
+    {
+        cout << "  Nenhum cliente registrado nesta transação." << endl;
+    }
+    else
+    {
+        for (Cliente *cliente : clientesEnvolvidos)
+        {
+            cout << "  - " << cliente->nome << endl; // o nome esta publico na classe pessoa
+        }
+    }
+}
+
+/*
+
+    void setClientes(Cliente *c);
+
+    */
