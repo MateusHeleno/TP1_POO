a67886fc5a2470e59cfc604acc430dac142baa46
Phenriqx <pedroxlbd@gmail.com>
2026-05-09 14:03:00 -0300

    mudança para getNome porque agora é um atributo protected

--- transacao.cpp
+++ transacao.cpp
@@ -1,5 +1,7 @@
 #include "transacao.hpp"
 #include "cliente.hpp"
+#include "pessoa.hpp"
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,65 +17,47 @@ Transacao::Transacao(string t, double v, string d, string h)
     // a classe foi inicializada acima
 }
 
-string Transacao::getTipo()
-{
-
+string Transacao::getTipo() {
     return tipo;
 }
 
-double Transacao::getValor()
-{
-
+double Transacao::getValor() {
     return valor;
 }
 
-void Transacao::setValor(double v)
-{
+void Transacao::setValor(double v) {
 
     if (v >= 0.001)
-    {
         valor = v;
-    }
     else
         cout << "Erro: Valor invalido";
 }
 
-string Transacao::getData()
-{
-
+string Transacao::getData() {
     return data;
 }
 
-void Transacao::setData(string d)
-{
-
+void Transacao::setData(string d) {
     data = d;
 }
 
-string Transacao::getHorario()
-{
-
+string Transacao::getHorario() {
     return horario;
 }
 
-void Transacao::setHorario(string h)
-{
-
+void Transacao::setHorario(string h) {
     horario = h;
 }
 
-void Transacao::setClientes(Cliente *c)
-{
+void Transacao::setClientes(Cliente *c) {
     clientesEnvolvidos.push_back(c);
 }
 
-vector<Cliente *> Transacao::getClientes()
-{
+vector<Cliente *> Transacao::getClientes() {
     return clientesEnvolvidos; // ja eh a colecao de clientes envolvidos completa
 }
 
-void Transacao::exibirTransacao()
-{
+void Transacao::exibirTransacao() {
     cout << "Tipo: " << tipo << endl
          << "Valor: R$ " << valor << endl
          << "Data: " << data << endl
@@ -82,14 +66,10 @@ void Transacao::exibirTransacao()
     cout << "Clientes Envolvidos:" << endl; // impressao dos clientes
 
     if (clientesEnvolvidos.empty()) // verifica se a lista esta vazia
-    {
         cout << "  Nenhum cliente registrado nesta transação." << endl;
-    }
     else
-    {
-        for (Cliente *cliente : clientesEnvolvidos)
-        {
-            cout << "  - " << cliente->nome << endl; // o nome esta publico na classe pessoa
+        for (Cliente *cliente : clientesEnvolvidos) {
+            string nome = cliente->getNome();
+            cout << "  - " << nome << endl; // o nome esta protegido na classe pessoa -> usamos getter
         }
-    }
 }
